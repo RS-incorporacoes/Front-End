7124aff2a7c39dce68c763deb9cdd9c918738057
pHBasic <pedrohenrriqueferreira@gmail.com>
2023-03-30 18:08:14 -0300

    feat[src/ap]: created land page and banner components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { LandPageComponent } from './land-page/land-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandPageComponent},
+  { path: 'projects', component: ProjectPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LandPageComponent } from './land-page/land-page.component';
+import { BannerComponent } from './banner/banner.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LandPageComponent
+    LandPageComponent,
+    BannerComponent,
+    ProjectPageComponent
   ],
   imports: [
     BrowserModule,
