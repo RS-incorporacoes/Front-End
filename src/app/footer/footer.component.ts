62ee8cd5960b69c573226e15309b8a7921bda01b
pHBasic <pedrohenrriqueferreira@gmail.com>
2023-04-15 00:00:49 -0300

    feat[src/app]: create button that scrolls to top

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  onActivate() {
+    // window.scroll(0,0);
+ 
+    window.scroll({ 
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth' 
+     });
+ }
 }
