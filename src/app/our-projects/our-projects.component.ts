c6b48f35519c4dd620544f108c95de7062867815
pHBasic <pedrohenrriqueferreira@gmail.com>
2023-04-19 11:39:06 -0300

    feat[src/app]: Changed the design of banner and our-history components

--- src/app/our-projects/our-projects.component.ts
+++ src/app/our-projects/our-projects.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-our-projects',
   templateUrl: './our-projects.component.html',
   styleUrls: ['./our-projects.component.css']
 })
-export class OurProjectsComponent {
 
+export class OurProjectsComponent {
+  baseURL = 'http://localhost:3000/projects'
+  imagePath = ['../../assets/images/171244.jpg','../../assets/images/171251.jpg','../../assets/images/171271.jpg','../../assets/images/darkforest.jpg','../../assets/images/1535930.jpg','../../assets/images/10838509.jpg']
+  centerImg = 1
+
+  async getProject(){
+   const res = await axios.get(this.baseURL)
+  }
+
+  nextImage(){
+    if(!(this.centerImg + 2 > this.imagePath.length - 1)){
+      this.centerImg++
+    }
+  }
+
+  prevImage(){
+    if(!(this.centerImg - 2 < 0)){
+      this.centerImg--
+    }
+  }
+  
+
+  voltar = '<'
+  prox = '>'
 }
