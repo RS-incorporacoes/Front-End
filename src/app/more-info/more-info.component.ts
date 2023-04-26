c36c064893f73038d3a4213fb0dc7506182663d3
pHBasic <pedrohenrriqueferreira@gmail.com>
2023-04-26 20:12:39 -0300

    fix[]: temporary database

--- src/app/more-info/more-info.component.ts
+++ src/app/more-info/more-info.component.ts
@@ -12,6 +12,52 @@ export class MoreInfoComponent implements OnInit, DoCheck {
   id: any
   projects: any
   baseURL = "http://localhost:3000/projects"
+  project = [{
+    "id": 1,
+    "name": "Loteamento Montanha Solene",
+    "address": "Povoado Lagoa Redonda - Pirambu",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, porttitor vitae pulvinar vel, sollicitudin nec ante. Aliquam vel elementum est. In tincidunt lacus a tincidunt lacinia. Fusce viverra tellus sapien. Sed eros est, commodo interdum suscipit non, elementum et risus. Quisque rutrum lacinia consequat. Praesent volutpat nibh eu vulputate ultrices. Phasellus feugiat tincidunt est a efficitur. Suspendisse ut justo in dui rhoncus condimentum eget a eros.",
+    "shotDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non.",
+    "price": "20000",
+    "img": "../../assets/images/171244.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Loteamento Riacho Doce",
+    "address": "Conjunto Augusto Franco - Aracaju",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, porttitor vitae pulvinar vel, sollicitudin nec ante. Aliquam vel elementum est. In tincidunt lacus a tincidunt lacinia. Fusce viverra tellus sapien. Sed eros est, commodo interdum suscipit non, elementum et risus. Quisque rutrum lacinia consequat. Praesent volutpat nibh eu vulputate ultrices. Phasellus feugiat tincidunt est a efficitur. Suspendisse ut justo in dui rhoncus condimentum eget a eros.",
+    "shotDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non.",
+    "price": "10000",
+    "img": "../../assets/images/171251.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Loteamento Jardim Florido",
+    "address": "Atalaia Nova - Barra dos Coqueiros",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, porttitor vitae pulvinar vel, sollicitudin nec ante. Aliquam vel elementum est. In tincidunt lacus a tincidunt lacinia. Fusce viverra tellus sapien. Sed eros est, commodo interdum suscipit non, elementum et risus. Quisque rutrum lacinia consequat. Praesent volutpat nibh eu vulputate ultrices. Phasellus feugiat tincidunt est a efficitur. Suspendisse ut justo in dui rhoncus condimentum eget a eros.",
+    "shotDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non.",
+    "price": "15000",
+    "img": "../../assets/images/171271.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Loteamento Riacho Doce",
+    "address": "Povoado Lagoa Redonda - Pirambu",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, porttitor vitae pulvinar vel, sollicitudin nec ante. Aliquam vel elementum est. In tincidunt lacus a tincidunt lacinia. Fusce viverra tellus sapien. Sed eros est, commodo interdum suscipit non, elementum et risus. Quisque rutrum lacinia consequat. Praesent volutpat nibh eu vulputate ultrices. Phasellus feugiat tincidunt est a efficitur. Suspendisse ut justo in dui rhoncus condimentum eget a eros.",
+    "shotDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non.",
+    "price": "20000",
+    "img": "../../assets/images/171244.jpg"
+  },
+  {
+    "id": 5,
+    "name": "Loteamento Riacho Doce",
+    "address": "Povoado Lagoa Redonda - Pirambu",
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, porttitor vitae pulvinar vel, sollicitudin nec ante. Aliquam vel elementum est. In tincidunt lacus a tincidunt lacinia. Fusce viverra tellus sapien. Sed eros est, commodo interdum suscipit non, elementum et risus. Quisque rutrum lacinia consequat. Praesent volutpat nibh eu vulputate ultrices. Phasellus feugiat tincidunt est a efficitur. Suspendisse ut justo in dui rhoncus condimentum eget a eros.",
+    "shotDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non.",
+    "price": "20000",
+    "img": "../../assets/images/171244.jpg"
+  }
+]
 
   async ngOnInit(){
     const res = await axios.get(this.baseURL)
--- src/app/our-projects/our-projects.component.ts
+++ src/app/our-projects/our-projects.component.ts
@@ -10,6 +10,53 @@ import axios from 'axios';
 export class OurProjectsComponent implements OnInit {
   baseURL = 'http://localhost:3000/projects'
   projects: any
+  project = [
+    {
+      "id": 1,
+      "name": "Loteamento Montanha Solene",
+      "address": "Povoado Lagoa Redonda - Pirambu",
+      "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, porttitor vitae pulvinar vel, sollicitudin nec ante. Aliquam vel elementum est. In tincidunt lacus a tincidunt lacinia. Fusce viverra tellus sapien. Sed eros est, commodo interdum suscipit non, elementum et risus. Quisque rutrum lacinia consequat. Praesent volutpat nibh eu vulputate ultrices. Phasellus feugiat tincidunt est a efficitur. Suspendisse ut justo in dui rhoncus condimentum eget a eros.",
+      "shotDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non.",
+      "price": "20000",
+      "img": "../../assets/images/171244.jpg"
+    },
+    {
+      "id": 2,
+      "name": "Loteamento Riacho Doce",
+      "address": "Conjunto Augusto Franco - Aracaju",
+      "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, porttitor vitae pulvinar vel, sollicitudin nec ante. Aliquam vel elementum est. In tincidunt lacus a tincidunt lacinia. Fusce viverra tellus sapien. Sed eros est, commodo interdum suscipit non, elementum et risus. Quisque rutrum lacinia consequat. Praesent volutpat nibh eu vulputate ultrices. Phasellus feugiat tincidunt est a efficitur. Suspendisse ut justo in dui rhoncus condimentum eget a eros.",
+      "shotDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non.",
+      "price": "10000",
+      "img": "../../assets/images/171251.jpg"
+    },
+    {
+      "id": 3,
+      "name": "Loteamento Jardim Florido",
+      "address": "Atalaia Nova - Barra dos Coqueiros",
+      "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, porttitor vitae pulvinar vel, sollicitudin nec ante. Aliquam vel elementum est. In tincidunt lacus a tincidunt lacinia. Fusce viverra tellus sapien. Sed eros est, commodo interdum suscipit non, elementum et risus. Quisque rutrum lacinia consequat. Praesent volutpat nibh eu vulputate ultrices. Phasellus feugiat tincidunt est a efficitur. Suspendisse ut justo in dui rhoncus condimentum eget a eros.",
+      "shotDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non.",
+      "price": "15000",
+      "img": "../../assets/images/171271.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Loteamento Riacho Doce",
+      "address": "Povoado Lagoa Redonda - Pirambu",
+      "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, porttitor vitae pulvinar vel, sollicitudin nec ante. Aliquam vel elementum est. In tincidunt lacus a tincidunt lacinia. Fusce viverra tellus sapien. Sed eros est, commodo interdum suscipit non, elementum et risus. Quisque rutrum lacinia consequat. Praesent volutpat nibh eu vulputate ultrices. Phasellus feugiat tincidunt est a efficitur. Suspendisse ut justo in dui rhoncus condimentum eget a eros.",
+      "shotDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non.",
+      "price": "20000",
+      "img": "../../assets/images/171244.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Loteamento Riacho Doce",
+      "address": "Povoado Lagoa Redonda - Pirambu",
+      "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros tellus, porttitor vitae pulvinar vel, sollicitudin nec ante. Aliquam vel elementum est. In tincidunt lacus a tincidunt lacinia. Fusce viverra tellus sapien. Sed eros est, commodo interdum suscipit non, elementum et risus. Quisque rutrum lacinia consequat. Praesent volutpat nibh eu vulputate ultrices. Phasellus feugiat tincidunt est a efficitur. Suspendisse ut justo in dui rhoncus condimentum eget a eros.",
+      "shotDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non.",
+      "price": "20000",
+      "img": "../../assets/images/171244.jpg"
+    }
+  ]
   centerImg = 1
 
   async ngOnInit(){
